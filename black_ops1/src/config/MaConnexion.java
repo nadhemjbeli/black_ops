a7e78c9bc4ba68a0dd81f49800af6d9a9172b255
Mokhtar <j.mokhtar@yahoo.fr>
2022-02-13 17:13:22 +0100

    crud : skin,jeu,champion,image

--- black_ops1/src/config/MaConnexion.java
+++ black_ops1/src/config/MaConnexion.java
@@ -1,18 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package black_ops.tools;
+
+package config;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-/**
- *
- * @author ASUS
- */
 public class MaConnexion {
     public String url="jdbc:mysql://localhost:3306/black_ops";
     public String user="root";
