fc5e3747ffadb8eded1c42f636672f636adaa001
Fritchou2 <mohamedaziz.ferchichi@esprit.tn>
2022-03-04 20:03:41 +0100

    comm

---
+++ src/black_ops/GUI_User/Competition/Defi/ViewListDefiController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package black_ops.GUI_User.Competition.Defi;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author medaz
+ */
+public class ViewListDefiController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
